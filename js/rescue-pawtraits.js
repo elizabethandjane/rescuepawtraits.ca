4721393ebc5702be72475fc168f3ac99fa0b4735
Thomas J Bradley <hey@thomasjbradley.ca>
2014-04-01 17:54:07 -0400

    Added functionality to load more automatically.

--- js/rescue-pawtraits.js
+++ js/rescue-pawtraits.js
@@ -1,8 +1,31 @@
-var container, msnry;
+var container, msnry, $btn, page = 1, loadMore, maxPages, $dogs;
+
+maxPages = $('.pager li:nth-last-child(2) a').attr('href').replace(/[^\d]/g, '');
+$dogs = $('.dogs');
+
+loadMore = function () {
+  var req;
+
+  page++;
+
+  if (page > maxPages) {
+    return;
+  }
+
+  req = $.ajax('/dog-photos-' + page + '/');
+  req.done(function (data) {
+    var dogs = $(data).find('.dog');
+
+    $dogs.append(dogs);
+
+    if ("Masonry" in window) {
+      $dogs.masonry('appended', dogs);
+    }
+  });
+};
 
 if ("Masonry" in window) {
-  container = document.querySelector('.dogs');
-  msnry = new Masonry(container, {
+  $dogs.masonry({
     columnWidth: 320,
     itemSelector: '.dog',
     gutter: 8,
@@ -10,3 +33,10 @@ if ("Masonry" in window) {
     isFitWidth: true
   });
 }
+
+$btn = $('<button class="load-btn">Load More</button>');
+$('.loader').html($btn);
+
+$btn.on('click touchend', function (e) {
+  loadMore();
+});
